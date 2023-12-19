5e32e2d299ab4a962b5cdd027e43c942871986af
Edna Omadjambe <ednaomadjambe@Ednas-Air.attlocal.net>
2023-12-19 07:03:07 -0500

    file rename

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './components/Home';
+import Home from './components/Main.js';
 
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
         <Routes>
 
             <Route
-            path="/eco-designs"
-            element={<Home />}
+            path="/ecco-designs"
+            element={<Main />}
             />
 
         </Routes>
